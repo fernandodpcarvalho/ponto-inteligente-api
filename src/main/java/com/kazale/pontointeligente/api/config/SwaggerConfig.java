6a1a6dbc788c1a7022a69a10b08accfcf827695b
Fernando <fernandodpcarvalho@gmail.com>
2018-09-12 06:53:13 -0300

    Adicionado Swagger

--- src/main/java/com/kazale/pontointeligente/api/config/SwaggerConfig.java
+++ src/main/java/com/kazale/pontointeligente/api/config/SwaggerConfig.java
@@ -9,83 +9,53 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.kazale.pontointeligente.api.security.utils.JwtTokenUtil;
 
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger.web.ApiKeyVehicle;
-//import springfox.documentation.swagger.web.SecurityConfiguration;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@Configuration
-//@Profile("dev")
-//@EnableSwagger2
+@Configuration
+@Profile("dev")
+@EnableSwagger2
 public class SwaggerConfig {
 
-//	@Autowired
-//	private JwtTokenUtil jwtTokenUtil;
-//
-//	@Autowired
-//	private UserDetailsService userDetailsService;
-//
-//	@Bean
-//	public Docket api() {
-//		return new Docket(DocumentationType.SWAGGER_2).select()
-//				.apis(RequestHandlerSelectors.basePackage("com.kazale.pontointeligente.api.controllers"))
-//				.paths(PathSelectors.any()).build()
-//				.apiInfo(apiInfo());
-//	}
-//
-//	private ApiInfo apiInfo() {
-//		return new ApiInfoBuilder().title("Ponto Inteligente API")
-//				.description("Documentação da API de acesso aos endpoints do Ponto Inteligente.").version("1.0")
-//				.build();
-//	}
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
 
-//	@Bean
-//	public SecurityConfiguration security() {
-//		String token;
-//		try {//	@Bean
-//	public SecurityConfiguration security() {@Autowired
-//	private JwtTokenUtil jwtTokenUtil;
-//
-//	@Autowired
-//	private UserDetailsService userDetailsService;
-//
-//	@Bean
-//	public Docket api() {
-//		return new Docket(DocumentationType.SWAGGER_2).select()
-//				.apis(RequestHandlerSelectors.basePackage("com.kazale.pontointeligente.api.controllers"))
-//				.paths(PathSelectors.any()).build()
-//				.apiInfo(apiInfo());
-//	}
-//
-//	private ApiInfo apiInfo() {
-//		return new ApiInfoBuilder().title("Ponto Inteligente API")
-//				.description("Documentação da API de acesso aos endpoints do Ponto Inteligente.").version("1.0")
-//				.build();
-//	}
-//	String token;
-//	try {
-//		UserDetails userDetails = this.userDetailsService.loadUserByUsername("admin@kazale.com");
-//		token = this.jwtTokenUtil.obterToken(userDetails);
-//	} catch (Exception e) {
-//		token = "";
-//	}
-//
-//	return new SecurityConfiguration(null, null, null, null, "Bearer " + token, ApiKeyVehicle.HEADER,
-//			"Authorization", ",");
-//}
-//			UserDetails userDetails = this.userDetailsService.loadUserByUsername("admin@kazale.com");
-//			token = this.jwtTokenUtil.obterToken(userDetails);
-//		} catch (Exception e) {
-//			token = "";
-//		}
-//
-//		return new SecurityConfiguration(null, null, null, null, "Bearer " + token, ApiKeyVehicle.HEADER,
-//				"Authorization", ",");
-//	}
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.kazale.pontointeligente.api.controllers"))
+				.paths(PathSelectors.any()).build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Ponto Inteligente API")
+				.description("Documentação da API de acesso aos endpoints do Ponto Inteligente.").version("1.0")
+				.build();
+	}
+
+	@Bean
+	public SecurityConfiguration security() {
+		String token;
+		try {
+			UserDetails userDetails = this.userDetailsService.loadUserByUsername("admin@kazale.com");
+			token = this.jwtTokenUtil.obterToken(userDetails);
+		} catch (Exception e) {
+			token = "";
+		}
+
+		return new SecurityConfiguration(null, null, null, null, "Bearer " + token, ApiKeyVehicle.HEADER,
+				"Authorization", ",");
+	}
 
 }
